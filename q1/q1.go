db30c1b9869b51e1414675ee29384213c1bf1cb7
joaoplima04 <126472676+joaoplima04@users.noreply.github.com>
2023-06-12 08:38:35 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -8,13 +8,14 @@ package q1
 //Você pode retornar a resposta em qualquer ordem.
 
 func TwoSum(nums []int, target int) []int {
-	var indice []int
-	for y:=0;y<len(nums);y++{
-		for x:=len(nums);x<=0;x--{
-			if nums[y] + nums[x] == target && x != y{
-				indice = append(indice, x, y)
+	indices := make([]int, 2)
+	for y := 0; y < len(nums); y++ {
+		for x := y + 1; x < len(nums); x++ {
+			if nums[x]+nums[y] == target {
+				indices[0] = y
+				indices[1] = x
 			}
 		}
 	}
-	return indice
+	return indices
 }
